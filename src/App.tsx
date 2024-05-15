d81bc9d11f556755d5ac4a44fd7bc84eb4c9b8dd
Wojciech Antos <wojciech.antos@teems.it>
2024-05-15 08:14:57 +0200

    Shadcn implemented with configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-function App() {
-	return <h1 className='text-7xl font-bold '>App</h1>;
-}
+import { Button } from '@/components/ui/button';
 
+function App() {
+	return (
+		<div>
+			<h1 className='text-7xl font-bold '>App</h1>
+			<Button variant='destructive' size='lg'>
+				Click Me
+			</Button>
+		</div>
+	);
+}
 export default App;
