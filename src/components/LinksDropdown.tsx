7b3341aedd4f9f1bbd1c528693e4139827c4f846
Wojciech Antos <wojciech.antos@teems.it>
2024-05-19 08:32:24 +0200

    Mobile dropdown menu in navbar implemented with pages navigation

--- src/components/LinksDropdown.tsx
+++ src/components/LinksDropdown.tsx
@@ -1,5 +1,37 @@
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { AlignLeft } from 'lucide-react';
+import { Button } from './ui/button';
+import { links } from '@/utils';
+import { NavLink } from 'react-router-dom';
+
 function LinksDropdown() {
-    return <div>LinksDropdown</div>;
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild className='lg:hidden'>
+                <Button variant='outline' size='icon'>
+                    <AlignLeft />
+                    <span className='sr-only'>Toggle Links</span>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className='w-52 lg:hidden' align='start' sideOffset={25}>
+                {links.map((link) => (
+                    <DropdownMenuItem key={link.label}>
+                        <NavLink
+                            to={link.href}
+                            className={(isActive) => `capitalize w-full ${isActive ? 'text-primary' : ''}`}
+                        >
+                            {link.label}
+                        </NavLink>
+                    </DropdownMenuItem>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
 }
 
 export default LinksDropdown;
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,12 @@
+import { Link } from 'react-router-dom';
+import { Armchair } from 'lucide-react';
+
 function Logo() {
-    return <div>Logo</div>;
+    return (
+        <Link to='/' className='hidden lg:flex justify-center items-center bg-primary p-2 rounded-lg text-white'>
+            <Armchair className='w-8 h-8' />
+        </Link>
+    );
 }
 
 export default Logo;
